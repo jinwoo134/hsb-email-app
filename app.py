de8f26e01c08518cffe49c591a06ca14d78571b4
jinwoo134 <jinwoo.jung.work@gmail.com>
2025-11-06 02:32:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-# app.py
 import os
 import base64
 import mimetypes
